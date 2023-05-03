2ee2eb04572947b831b77122a876b4058a84b2ca
angadsinghsandhu <angadsandhuworkmail@gmail.com>
2023-05-03 17:55:19 +0530

    initialized Bigram methods

--- Youtube/Andrej Karpathy/Let's build GPT/code/main.py
+++ Youtube/Andrej Karpathy/Let's build GPT/code/main.py
@@ -58,3 +58,10 @@ def generate_gpt():
     context = torch.zeros((1, 1), dtype=torch.long, device=device)
     print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
     # open('./data/sample_output.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
+
+# TODO: Complete Bigram methods
+def train_bigram():
+    pass
+
+def generate_bigram():
+    pass
